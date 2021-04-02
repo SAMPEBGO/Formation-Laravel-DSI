ce5716cdccc3a4a60fd35d98c00a4a9e74470e62
SAMPEBGO <asampebgo@gmail.com>
2021-04-02 12:07:13 +0000

    update_procfiles_02

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\Url;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         
-        Url::forceScheme('https');
+        URL::forceScheme('https');
         //permet de prendre la pagination de bootstrap par defaut
         Paginator::useBootstrap();
 
